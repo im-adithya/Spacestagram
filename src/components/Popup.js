7dff865c94183ddaf5cf5005af28657d8c0ab1f3
Adithya Vardhan <imadithyavardhan@gmail.com>
2022-01-19 03:47:11 +0530

    fix: popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -5,7 +5,7 @@ import loader from '../assets/loader.svg';
 
 const Loader = ({ message }) => {
   return (
-    <div className="position-fixed bottom-0 start-0 py-2 px-3 m-md-4 d-flex justify-content-center align-items-center popup">
+    <div className="position-fixed bottom-0 start-0 py-2 px-3 m-2 m-md-4 d-flex justify-content-center align-items-center popup">
       <Image src={loader} width="30px" />
       <p className="mb-0 text-18 ml-2">{message}</p>
     </div>
